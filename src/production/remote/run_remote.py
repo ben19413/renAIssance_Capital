9af51d64817cb86ff8087239b185c70955de39f5
ben19413 <benjones19413@gmail.com>
2025-01-28 19:56:39 +0000

    account for different remote and local workflows

---
+++ src/production/remote/run_remote.py
@@ -0,0 +1,16 @@
+# retrieve price data via:
+# if deploy_remote:
+#     api call to trading bot 
+# else:
+#     read local csv 
+
+# make features
+# call train classifier (input: data) (output: buy, sell, no trade)
+# call train ATR (input: data) (output: stop loss)
+
+# Return prediction, stop loss, take profit via:
+# if deploy_remote:
+#     responding to API call 
+# else:
+#     make new local csv 
+
