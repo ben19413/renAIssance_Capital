bed281980c623951d0b2ee2bafd42599b1683bb3
ben19413 <benjones19413@gmail.com>
2025-02-05 08:30:30 +0000

    initiate loop

--- src/backtesting/get_backtesting_data.py
+++ src/backtesting/get_backtesting_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 # TODO: REPLACE HACK WITH LIVE DATA SOURCE
 
 def get_backtesting_data():
-    full_backtesting_data = pd.read_csv('src/files/dummy_BTC_dev_data.csv')
+    full_backtesting_data = pd.read_csv('../files/dummy_BTC_dev_data.csv')
 
     return full_backtesting_data
 
--- src/backtesting/run.py
+++ src/backtesting/run.py
@@ -1,15 +1,34 @@
 from production.make_features import make_features
 from production.models.ATR import ATR
 from production.models.classifier import classifier
-#from backtesting import get_backtesting_data
+from backtesting.get_backtesting_data import get_backtesting_data
 import pandas as pd
 
+# TODO: replace with variables straight from .env 
+training_period_data_size = 1000
+# format YYYY-MM-DD HH:MM
+backtest_start_date_time = "2024.05.26 18:00"
+# format YYYY-MM-DD HH:MM
+backtest_end_date_time = "2024.05.26 20:00"
 
-# get full backtesting data via get_backtesting_data
-#results_df = pd.DataFrame()
-print("IT WORKED")
+# TODO: adapt this to initiate correct columns
+results_df = pd.DataFrame()
 
-#full_backtesting_data = get_backtesting_data()
+full_backtesting_data = get_backtesting_data().iloc[::-1].reset_index(drop=True)
+
+# Get first start point and final start point
+first_training_end_index = full_backtesting_data.index[full_backtesting_data['Time'] == backtest_start_date_time].tolist()[0]
+final_training_end_index = full_backtesting_data.index[full_backtesting_data['Time'] == backtest_end_date_time].tolist()[0]
+
+for training_end_point in range(first_training_end_index, final_training_end_index + 1):
+
+    training_start_point = training_end_point - training_period_data_size + 1
+    training_df = full_backtesting_data.iloc[training_start_point:training_end_point + 1]
+
+    print(training_df)
+
+
+    
 
 
 
