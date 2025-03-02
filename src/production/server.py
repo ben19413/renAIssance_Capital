c5d871c5ffee9fc6294e7b27372872027ffa9133
ben19413 <benjones19413@gmail.com>
2025-03-02 22:44:22 +0000

    changes

--- src/production/server.py
+++ src/production/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
@@ -12,8 +12,32 @@ from production.remote.process_api_data import process_api_data
 from production.make_features import make_features
 from production.models.classifier import classifier
 from production.models.ATR import ATR_prod_refactor
-
+import logging
 app = FastAPI()
+
+import sys
+import logging
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Any
+
+# Configure logging
+logger = logging.getLogger("my_app")  # Custom logger name
+logger.setLevel(logging.DEBUG)  # Set logging level to DEBUG
+
+# Create a handler that logs to stdout (Docker will capture this)
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(logging.DEBUG)
+
+# Define a log format
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s")
+stream_handler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(stream_handler)
+
+# Prevent duplicate log entries by removing default handlers
+logger.propagate = False
 
 CONFIG_PATH = "/app/production/config_production.json"
 
@@ -27,10 +51,34 @@ def read_root():
 
 
 @app.post('/train_and_predict')
-def train_and_predict(json: dict):
+async def train_and_predict(request: Request):
+    logger.info("POST request received")
+    body = await request.body()
 
-    live_df = process_api_data(json)
-    features_df = make_features(live_df)
+    
+    #logger.info("Raw body bytes: %s", body_bytes)
+
+    # logger.info(body)
+
+    body_str = body.decode('utf-8')
+    body_str = body_str.lstrip('\x00')
+    logger.info(f"Body length: {len(body_str)}")
+    body_str = body_str.strip()
+
+    logger.info(f"Body length after strip: {len(body_str)}")
+    logger.info(f"Raw body content: {body_str}")
+    logger.info("PREJSONPARSE")
+    logger.info(body_str)
+    logger.info(type(body_str))
+
+    body_str = json.loads(body_str) 
+
+    logger.info("POSTJSONPARSE")
+    logger.info(body_str)
+    logger.info(type(body_str))
+
+    live_df = process_api_data(body_str)
+    features_df = make_features(live_df, config["target_width"])
 
     trade = classifier(features_df)
 
