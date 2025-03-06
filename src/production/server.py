e3c6420f66821924a4cda0a1d10ae3cd49ed80c7
ben19413 <benjones19413@gmail.com>
2025-03-06 20:03:25 +0000

    push

--- src/production/server.py
+++ src/production/server.py
@@ -12,32 +12,9 @@ from production.remote.process_api_data import process_api_data
 from production.make_features import make_features
 from production.models.classifier import classifier
 from production.models.ATR import ATR
-import logging
+
 app = FastAPI()
 
-import sys
-import logging
-from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Any
-
-# Configure logging
-logger = logging.getLogger("my_app")  # Custom logger name
-logger.setLevel(logging.DEBUG)  # Set logging level to DEBUG
-
-# Create a handler that logs to stdout (Docker will capture this)
-stream_handler = logging.StreamHandler(sys.stdout)
-stream_handler.setLevel(logging.DEBUG)
-
-# Define a log format
-formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s")
-stream_handler.setFormatter(formatter)
-
-# Add the handler to the logger
-logger.addHandler(stream_handler)
-
-# Prevent duplicate log entries by removing default handlers
-logger.propagate = False
 
 CONFIG_PATH = "/app/production/config_production.json"
 
@@ -52,15 +29,14 @@ def read_root():
 
 @app.post('/train_and_predict')
 async def train_and_predict(request: Request):
-    
     body = await request.body()
     body_str = body.decode('utf-8')
     body_str = body_str.lstrip('\x00')
     body_str = body_str.strip()
     body_str = json.loads(body_str) 
     live_df = process_api_data(body_str)
+    live_df.index = pd.to_datetime(live_df["Time"])
     features_df = make_features(live_df, config["target_width"])
-    features_df.index = pd.to_datetime(features_df["Time"])
     trade = classifier(features_df)
 
     prediction_df = pd.DataFrame(
@@ -71,17 +47,21 @@ async def train_and_predict(request: Request):
     prediction_df.index = features_df.tail(1).index
 
     orders_df = ATR(features_df, prediction_df, config["risk_to_reward_ratio"])
-    
 
     time = str(orders_df.tail(1).index[0])
-    stop_loss = float(orders_df.tail(1)["stop_loss"])
-    take_profit = float(orders_df.tail(1)["take_profit"])
-    atr = float(orders_df.tail(1)["ATR"])
+    stop_loss = float(orders_df.tail(1)["stop_loss"].iloc[0])
+    take_profit = float(orders_df.tail(1)["take_profit"].iloc[0])
+    atr = float(orders_df.tail(1)["ATR"].iloc[0])
+
+    if trade == 0:
+        stop_loss = 0
+        take_profit = 0
+        atr = 0
 
     return {
         "time": time,
         "trade": trade,
-        "stop_loss": stop_loss,
-        "take_profit": take_profit,
-        "ATR": atr
+        "stop_loss": stop_loss if stop_loss is not np.nan else 0,
+        "take_profit": take_profit if take_profit is not np.nan else 0,
+        "ATR": atr if atr is not np.nan else 0
     }
